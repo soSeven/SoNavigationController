1a1eee06d524fe7f7dd90e42ef4c87f65cfda636
liqi <1521224526@qq.com>
2022-02-18 16:52:16 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-SoNavigationController_Example/Pods-SoNavigationController_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SoNavigationController_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SoNavigationController_Example
+@end
---
+++ Example/Pods/Target Support Files/SoNavigationController/SoNavigationController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SoNavigationController : NSObject
+@end
+@implementation PodsDummy_SoNavigationController
+@end
